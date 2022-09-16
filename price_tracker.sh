2bac56cdd8da5de09c022c13f45887ae0ad667ec
crankiz <svedberg.tobias@gmail.com>
2022-09-16 14:30:39 +0200

    Converted to shell scritp
    This is a bash script that request Vattenfalls API to get current kilowatt prices.

---
+++ price_tracker.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Check for dependencies
+if ! which jq > /dev/null; then
+   echo -e "jq not found! Install? (y/n) \c"
+   read -r
+   if [ "$REPLY" = "y" ]; then
+      sudo apt-get install jq
+   else
+      exit 1
+   fi
+fi
+
+# Get date and time
+today=$(date +%Y-%m-%d)
+time=$(date +%H)
+
+# API configuration
+api_base_url="https://www.vattenfall.se/api"
+
+# Postal code is used to get you price area
+# Change to you postal code
+postal_code="60214"
+
+# You can hard code your price area to limit your request count to the API
+# pricearea="SN1" # Norra Sverige
+# pricearea="SN2" # Norra Mellan Sverige
+# pricearea="SN3" # Södra Mellan Sverige
+# pricearea="SN4" # Södra Sverige
+pricearea=$(curl -s "${api_base_url}/priceareainfo/zipcode/${postal_code}" | jq -j .priceArea)
+
+# Get todays kilowatt prices
+response=$(curl -s "${api_base_url}/price/spot/pricearea/${today}/${today}/${pricearea}")
+price=$(jq -j ".[${time##0}].Value" <<< "${response}")
+unit=$(jq -j ".[${time##0}].Unit" <<< "${response}")
+
+# Return current hour kilowatt price 
+printf "%s %s\n" "${price}" "${unit}"
